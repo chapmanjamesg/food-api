145a9cf085925c144fb2d4f6c96204acbae9ff71
James Chapman <chapmanjamesg@gmail.com>
2019-10-17 11:29:10 -0500

    finished after I figured out that my b in Barcode wasn't capitalized

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -6,6 +6,11 @@ const foodFactory = (food) => {
         <h1>${food.name}</h1>
         <section>${food.category}<section>
         <aside>${food.ethnicity}</aside>
+        <p>${food.ingredients}</p>
+        <p>${food.country}</p>
+        <p>energy per serving: ${food.energy}</p>
+        <p>sugar per serving: ${food.sugar}</p>
+        <p>fat per serving: ${food.fat}</p>
     </div>
     `
 }
@@ -18,14 +23,45 @@ fetch("http://localhost:8088/food")
     //display json data in the console
     .then(parsedFoods => {
         console.table(parsedFoods)
-
+        
         parsedFoods.forEach(food => {
             //console.log('food', food)
             // console.log(food.name)
-            const addFoodToDom = document.querySelector('.foodList');
-            //you don't need a for loop here because it is an object not an array at this point.
-            addFoodToDom.innerHTML += foodFactory(food)
+            //make sure that your callbacks have similar caps
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.Barcode}.json`)
+            .then(response => response.json())
+            .then(productInfo => {
+                console.log(productInfo.product.ingredients_text)
+                if(productInfo.product.ingredients_text){
+                    food.ingredients = productInfo.product.ingredients_text
+                }else {
+                    food.ingredients = "no ingredients listed"
+                }
+                 if(productInfo.product.countries_hierarchy){
+                     food.country = productInfo.product.countries_hierarchy
+                 }else {
+                     food.country = "no country listed"
+                 }
+                 if(productInfo.product.nutriments.energy_serving){
+                     food.energy = productInfo.product.nutriments.energy_serving
+                 }else {
+                     food.energy = "no energy per serving listed"
+                 }
+                 if(productInfo.product.nutriments.sugars_serving){
+                     food.sugar = productInfo.product.nutriments.sugars_serving
+                 }else{
+                     food.sugar = "no sugar per serving listed"
+                 }
+                 if(productInfo.product.nutriments.fat_serving){
+                     food.fat = productInfo.product.nutriments.fat_serving
+                 }else {
+                     food.fat = "no fat per serving listed"
+                 }
+                const addFoodToDom = document.querySelector('.foodList');
+                //you don't need a for loop here because it is an object not an array at this point.
+                addFoodToDom.innerHTML += foodFactory(food)
 
+                })
 
         });
     })
